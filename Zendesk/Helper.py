ae926e08dcba9bf519783732b1b775574ec156de
Aung Yee <lyndon.lso@gmail.com>
2022-03-25 12:08:59 +0000

    added requester name field to zendesk ticket payload

--- Zendesk/Helper.py
+++ Zendesk/Helper.py
@@ -7,6 +7,7 @@ def createPayloadFromTask(taskDetail, group):
     if int(taskDetail['list']['id']) == LIST_ID['CUSTOMER_SUPPORT_TECHNICAL']:
 
         requestor = None
+        requestorName = None
         urgency = None
         category = None
         subcategory = None
@@ -26,12 +27,14 @@ def createPayloadFromTask(taskDetail, group):
                 subcategory = TECH_SUPPORT_CUSTOM_FIELDS['SUPPORT_SUB_CATEGORY']['values'][field['value']]
             if field['id'] == TECH_SUPPORT_CUSTOM_FIELDS['URGENCY']['id'] and 'value' in field:
                 urgency = TECH_SUPPORT_CUSTOM_FIELDS['URGENCY']['values'][field['value']]
+            if field['id'] == TECH_SUPPORT_CUSTOM_FIELDS['USER_NAME']['id'] and 'value' in field:
+                requestorName = field['value']
 
         payload = {
             'ticket': {
                 'comment': {
                     'body': f"""
-                        {taskDetail['creator']['username']} has requested a technical support task in in ClickUp.
+                        {taskDetail['creator']['username']} has requested a technical support task for {requestorName} in in ClickUp.
                         Check it out at: {taskDetail['url']}
 
                         Description:
@@ -39,6 +42,7 @@ def createPayloadFromTask(taskDetail, group):
                         
                         Details:
                         Urgency     : {urgency if urgency is not None else 'Nil'}
+                        Name        : {requestorName if requestorName is not None else 'Nil'}
                         Requester   : {requestor if requestor is not None else 'Nil'}
                         Booking Date: {bookingDate if bookingDate is not None else 'Nil'}
                         Booking Type: {bookingType if bookingType is not None else 'Nil'}
@@ -52,7 +56,8 @@ def createPayloadFromTask(taskDetail, group):
                 'subject': taskDetail['name'],
                 'description': taskDetail['description'],
                 'priority': taskDetail['priority']['priority'] if taskDetail['priority'] is not None else None,
-                "requester": {'email': requestor if requestor is not None else taskDetail['creator']['email']}
+                "requester": {'email': requestor if requestor is not None else taskDetail['creator']['email'],
+                              'name': requestorName if requestorName is not None else f"New Zendesk Customer<{taskDetail['creator']['email']}>"}
             }
         }
 
